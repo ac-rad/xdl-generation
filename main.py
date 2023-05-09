f537a5d08631aaa89e917d9cff9c55a98a53d8ba
martaskrt <skreta.marta@gmail.com>
2023-05-09 14:03:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -24,4 +24,4 @@ def index():
     return render_template("index.html", input_xdl=None, output_xdl=None)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=3000)
