93c459d1f987007504eb8fc85b46089fd8be9b8b
SebA-R <123127567+SebA-R@users.noreply.github.com>
2023-08-03 15:29:54 -0400

    Different models

--- main.py
+++ main.py
@@ -9,27 +9,43 @@ from flask_socketio import SocketIO, emit
 from verify import verify_xdl
 
 
-def prompt(instructions, description, max_tokens):
+def prompt(instructions, description, max_tokens, model="text-davinci-003"):
     """Function that calls the OpenAI API"""
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=description + "\nConvert to XDL:\n" + instructions,
-        temperature=0,
-        max_tokens=max_tokens,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-    )
-    return response["choices"][0]["text"]
+    if model == "text-davinci-003":
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=description + "\nConvert to XDL:\n" + instructions,
+            temperature=0,
+            max_tokens=max_tokens,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+        )
+        return response["choices"][0]["text"]
+    
+    elif model == "gpt-3.5-turbo" or model == "gpt-4":
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages = [
+                {"role":"system", "content":"You are a natural language to XDL translator, you must also do your best to correct any incorrect XDL, only use items contained in the description, here is a description of XDL:\n"+description },
+                {"role":"user", "content": f"\nConvert/Correct the following to proper XDL:\n{instructions}"}
+            ],
+            temperature=0,
+            max_tokens=max_tokens,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+        )
+        return response["choices"][0]["message"]["content"]
 
 
-def translate(input_xdl):
+
+def translate(input_xdl, model):
     """Function that translates the input XDL"""
 
-     # Get API key
-    
     openai.api_key = os.environ["OPENAI_API_KEY"]
-
+    openai.organization = os.environ["OPENAI_ORGANIZATION_ID"]
+    
     # Get XDL description
     with open("XDL_description.txt", "r") as f:
         XDL_description = f.read()
@@ -38,21 +54,22 @@ def translate(input_xdl):
     errors = {}
     prev_input_xdl = input_xdl
 
+    print("\033[32m" + "Model: " + model + "\033[0m")
+
     # Start 10 iteration for loop to limit token usage
     for step in range(10):
         socketio.emit("message", f"Convert to XDL: {input_xdl}")
         try:
-            gpt3_output = prompt(input_xdl, XDL_description, 1000)
+            gpt3_output = prompt(input_xdl, XDL_description, 1000, model)
         except:
-            socketio.emit(
-                "message", "Error. Too many tokens required or invalid API key.")
+            socketio.emit("message", "Error. Too many tokens required or invalid API key.")
             break
 
-        socketio.emit("message", "gpt3_output:::")
-        socketio.emit("message_xdl", f"{gpt3_output}")
-
         if "<XDL>" in gpt3_output:
-            gpt3_output = gpt3_output[gpt3_output.index("<XDL>"):]
+            gpt3_output = gpt3_output[gpt3_output.index("<XDL>"):gpt3_output.rindex("</XDL>") + 6]
+            print(gpt3_output)
+            socketio.emit("message", "gpt3_output:::")
+            socketio.emit("message_xdl", f"{gpt3_output}")
             compile_correct = verify_xdl(gpt3_output)
             errors[step] = {
                 "errors": compile_correct,
@@ -71,7 +88,7 @@ def translate(input_xdl):
                 input_xdl = f"{prev_input_xdl} {error_message}"
 
         else:
-            error_message = f"\n{gpt3_output}\nThis XDL was not correct. XDL should start with <XDL>. Please fix the errors."
+            error_message = f"\n{gpt3_output}\nThis XDL was not correct. XDL should start with <XDL> and end with </XDL>. Please fix the errors."
             input_xdl = f"{prev_input_xdl} {error_message}"
 
     try:
@@ -100,11 +117,11 @@ thread_lock = Lock()
 input_xdl = ""
 
 
-def run_translation(input_xdl):
+def run_translation(input_xdl, model):
     """Function that runs the translation in a separate thread."""
     global thread
     with thread_lock:
-        thread = Thread(target=translate, args=(input_xdl,))
+        thread = Thread(target=translate, args=(input_xdl, model))
         thread.start()
 
 
@@ -114,9 +131,9 @@ def index():
     global input_xdl
     if request.method == "POST":
         input_xdl = request.form["input_field"]
-        run_translation(input_xdl)
+        run_translation(input_xdl, request.form["gptVersion"])
 
     return render_template("index.html", input_xdl=input_xdl)
 
 
-socketio.run(app, port=3000)
+socketio.run(app, debug=True, port=3000)
